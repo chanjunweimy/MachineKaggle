0bd1ee2b939db52c991d63ec32f5fcd3a608b204
Chan Jun Wei <chanjunweimy@gmail.com>
2016-03-10 22:04:55 -0800

    try param
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- problem2.py
+++ problem2.py
@@ -4,27 +4,41 @@ import mltools as ml
 import mltools.linear
 import os 
 
+def convertX(X1):
+    n,m = X1.shape;
+
+    prob_x = []
+    
+    for i in range (n):
+        xi = {};
+        for j in range (m):
+            xi[j] = X1[i,j];
+        prob_x += [xi];
+    return prob_x;
+
 #os.chdir('/Users/CARA/Desktop/libsvm-3-2.21')
 
 from svmutil import *
 
-y, x = svm_read_problem('data/heart_scale')
 X1 = np.genfromtxt("data/kaggle.X1.train.txt",delimiter=',')
 Y = np.genfromtxt("data/kaggle.Y.train.txt",delimiter=',')
+Xte = np.genfromtxt("data/kaggle.X1.test.txt",delimiter=',')
+    
+prob_x = convertX(X1);
+pred_x = convertX(Xte);
 
-#print x;
-print X1.shape;
-
-n,m = X1.shape;
-prob_x = []
-
-for i in range (n):
-    xi = {};
-    for j in range (m):
-        xi[j] = X1[i,j];
-    prob_x += [xi];
-
+y = np.zeros(Xte.shape[0]);
+    
 prob = svm_problem(Y, prob_x)
-m = svm_train(prob,'-s 4')
-p_label, p_acc, p_val = svm_predict(Y, X1, m)
 
+print ("start svm training");
+
+m = svm_train(prob,'-s 4 -t 0 -n 0.5 -c 1')
+p_label, p_acc, p_val = svm_predict(y, pred_x, m)
+# print(len(p_val))
+fh = open('predictions0.csv','w')    # open file for upload
+fh.write('ID,Prediction\n')         # output header line
+for i,fake_yi in enumerate(p_val):
+  fh.write('{},{}\n'.format(i+1,fake_yi)) # output each prediction
+fh.close()
+    
